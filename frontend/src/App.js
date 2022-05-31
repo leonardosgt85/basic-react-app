aa1409cbabc961adc4e838b4c02d156b3768313a
leosgt85 <brunosilva@pm.mt.gov.br>
2022-05-31 18:10:38 -0400

    Login_andamento

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './global.css'
-import Header from './components/Header';
 import RoutesFunction from './routes';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <RoutesFunction />
     </div>
   );
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,63 @@
 import React from 'react'
 import './style.css'
+import LockIcon from '@mui/icons-material/Lock';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Brasao from '../../assets/brasaoDaci.gif';
 
 export default function Login(){
     return(
-        <div>
-            Página de Login
+        <div id='id_container_page_1'>
+            <div id= 'id_title_1' className='divCenter_1'>
+                <h1>S.F.I.C.I</h1>
+            </div>
+            <div id= 'id_brasao_l' className='divCenter_1'>
+                <img src={Brasao} alt='some value' width='10%'/>
+            </div>
+
+            <div id= 'id_lock' className='divCenter_1'>
+                <LockIcon fontSize = 'large'/>
+            </div>
+
+            <div id='id_containerBox_1'>
+            <Box
+            
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+
+            <div>
+
+            <div className = 'divCenter_1'>
+                <TextField
+                required
+                id="outlined-required"
+                label="Login"
+                defaultValue=""
+                
+                />
+            </div>
+
+            <div className = 'divCenter_1'>
+            <TextField
+                required
+                id="outlined-required"
+                label="Password"
+                defaultValue=""
+                />
+            </div>
+            </div>
+            <div id='id_button_1' className = 'divCenter_1'>
+            <Button variant="contained" size="small">Enviar</Button>
+            </div>
+            </Box>
+        </div>
+
         </div>
     )
 }
