77afdcd4951dc493719799ab5ff325ae20f3ae34
leosgt85 <brunosilva@pm.mt.gov.br>
2022-06-08 17:40:20 -0400

    criptografia example

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
+var CryptoJS = require("crypto-js");
 
+
+
+var ciphertext = CryptoJS.AES.encrypt('testes', 'secret key 123').toString();
+
+console.log(ciphertext)
+
+var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
+
+var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+
+console.log(originalText)
 
 const app = express();
-console.log('teste')
 app.use(express.json());
 app.use(routes);
 
