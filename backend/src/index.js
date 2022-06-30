230af3097ea5831348c25b7e58b1dc97bdfb1968
leosgt85 <brunosilva@pm.mt.gov.br>
2022-06-30 15:43:51 -0400

    implementação cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
-
+//O CORS está sendo utilizado para driblar o proxy.
+//a linha abaixo faz referência ao uso do CORS PARA DESENVOLVIMENTO, DEVE SER RETIRADA EM PRODUÇÃO
+const cors=require("cors");
 //********************************************CRYPTO*************************************************** */
 //#############instanciando o Crypto
 var CryptoJS = require("crypto-js");
@@ -14,8 +16,18 @@ var originalText = bytes.toString(CryptoJS.enc.Utf8);
 //console.log(originalText)
 //*****************************************CRYPTO_FIM************************************************** */
 
-
 const app = express();
+
+//cors, remover a constante abaixo
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+//cors, remover em produção
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
+
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost:3333',
+    baseURL:'http://172.16.75.175:3333',
 })
 
 export default api;
